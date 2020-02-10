56668f3ced95048efe2a1f365ddf32c230caaab0
procyontao <procyontao@gmail.com>
2020-02-10 15:21:49 -0800

    fix even odd training data are the same

--- bin/reconstruct_even_odd.py
+++ bin/reconstruct_even_odd.py
@@ -8,6 +8,7 @@ def tilt(tiltSeries, output, tiltfile, exclude_list):
             -THICKNESS {} \
             -RADIAL 0.35,0.035 \
             -RotateBy90 \
+            -FakeSIRTiterations 5 \
             -EXCLUDELIST2 {}".format(tiltSeries, output, tiltfile,200, exclude_list)
     print(s)
     call(s, shell=True)
@@ -18,11 +19,11 @@ def generate_tomograms(tiltSeries, tiltAngle):
     tilt_angle = np.loadtxt(tiltAngle)
     num_of_images=len(tilt_angle)
     if num_of_images%2==0:
-        even_list= [2*(i+1) for i in range(num_of_images/2)]
-    	odd_list= [2*i+1 for i in range(num_of_images/2)]
+        even_list= [2*(i+1) for i in range(num_of_images//2)]
+        odd_list= [2*i+1 for i in range(num_of_images//2)]
     else:
-        even_list= [2*(i+1) for i in range((num_of_images-1)/2-1)]
-        odd_list= [2*i+1 for i in range((num_of_images-1)/2)]
+        even_list= [2*(i+1) for i in range((num_of_images-1)//2-1)]
+        odd_list= [2*i+1 for i in range((num_of_images-1)//2)]
     output="odd.mrc"
     tilt(tiltSeries, "odd.mrc", tiltAngle, ','.join(map(str,even_list)))
     tilt(tiltSeries, "even.mrc", tiltAngle, ','.join(map(str,odd_list)))
