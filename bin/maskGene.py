a8aac3567356c25128c20583e8594baf2487ed35
Heng-Z <zh1242914414@mail.ustc.edu.cn>
2020-07-13 19:16:34 +0800

    noise generation and mask 1 line

--- bin/maskGene.py
+++ bin/maskGene.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
+
 import sys
 import mrcfile
 args = sys.argv
 from mwr.util.filter import stdmask_mpi
 if __name__ == "__main__":
-
+# the first arg is tomo name the second is mask name
     with mrcfile.open(args[1]) as n:
         tomo = n.data
     mask = stdmask_mpi(tomo,cubelen=20,cubesize=80,ncpu=20,if_rescale=True)
     with mrcfile.new(args[2],overwrite=True) as n:
         n.set_data(mask)
+
